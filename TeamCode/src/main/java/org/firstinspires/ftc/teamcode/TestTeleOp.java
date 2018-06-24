c25d7569cf19c7b17f9f250662502e5115e64df8
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-06-24 16:12:34 -0400

    Added hardware class usage and drive motor controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -10,12 +10,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Test TeleOp", group = "Testing")
 public class TestTeleOp extends OpMode {
 
-    public void init() {
+    TestHardware hardware = new TestHardware();
 
+    public void init() {
+        hardware.init(hardwareMap);
     }
 
     public void loop() {
         telemetry.addLine("Is is thing working?");
+
+        hardware.leftMotor  .setPower(gamepad1.left_stick_y);
+        hardware.rightMotor .setPower(gamepad1.right_stick_y);
     }
 
 }
