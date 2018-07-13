b1674e90e7e1914c5f05f0f848738dd2fa4f678d
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-07-12 20:29:58 -0400

    Added test drive code for TestAutonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -1,8 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by SwampbotsAdmin on 7/8/2018.
  */
 
-public class TestAutonomous {
+public class TestAutonomous extends LinearOpMode {
+
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+
+    public void runOpMode() {
+
+        leftDrive = hardwareMap.dcMotor.get("left_motor");
+        rightDrive = hardwareMap.dcMotor.get("right_motor");
+
+        telemetry.addLine("Ready.");
+        telemetry.addLine("Press play to start.");
+        telemetry.update();
+
+
+        waitForStart();
+
+        telemetry.addLine("Driving forward...");
+        telemetry.update();
+
+        leftDrive.setPower(0.5);
+        rightDrive.setPower(0.5);
+
+        sleep(2000);
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        telemetry.addLine("Finished.");
+        telemetry.update();
+    }
 }
