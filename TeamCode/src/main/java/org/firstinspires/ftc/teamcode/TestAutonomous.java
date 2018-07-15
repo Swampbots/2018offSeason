1288f2d6b2701bd4e2a70a59c4094de25de6a0e1
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-07-15 19:18:22 -0400

    Added registry annotation and reversed leftDrive motor direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -1,22 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by SwampbotsAdmin on 7/8/2018.
  */
 
+@Autonomous(name = "Test Autonomous", group = "Testing")
 public class TestAutonomous extends LinearOpMode {
 
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
 
+
     public void runOpMode() {
 
         leftDrive = hardwareMap.dcMotor.get("left_motor");
         rightDrive = hardwareMap.dcMotor.get("right_motor");
+
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addLine("Ready.");
         telemetry.addLine("Press play to start.");
@@ -36,8 +41,9 @@ public class TestAutonomous extends LinearOpMode {
         leftDrive.setPower(0);
         rightDrive.setPower(0);
 
-
         telemetry.addLine("Finished.");
         telemetry.update();
+
+        while(opModeIsActive()) {}
     }
 }
