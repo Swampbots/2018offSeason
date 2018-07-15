fee179cc281d748e56eb624c653db5d0398e485c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-07-15 19:49:48 -0400

    Added speed button logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -14,7 +14,9 @@ public class TestTeleOp extends OpMode {
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
 
-    public double driverSpeedMod = 0.2;
+    public final double SLOW = 0.1;
+    public final double NORMAL = 0.2;
+    public final double FAST = 0.4;
 
     public void init() {
         leftDrive = hardwareMap.dcMotor.get("left_motor");
@@ -24,9 +26,14 @@ public class TestTeleOp extends OpMode {
     }
 
     public void loop() {
+
+        double driverSpeedMod;
+
+        if(gamepad1.left_bumper)        driverSpeedMod = FAST;
+        else if(gamepad1.right_bumper)  driverSpeedMod = SLOW;
+        else                            driverSpeedMod = NORMAL;
+
         leftDrive.setPower(gamepad1.left_stick_y * driverSpeedMod);
         rightDrive.setPower(gamepad1.right_stick_y * driverSpeedMod);
     }
-
-
 }
