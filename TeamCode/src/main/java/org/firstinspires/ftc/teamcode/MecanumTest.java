f24b6f2b1f7d991978c50e35c0e2ecdcc5f7a31c
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-09-16 18:49:39 -0400

    Implemented MecanumHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
 /**
  * Created by SwampbotsAdmin on 8/19/2018.
  */
@@ -12,33 +13,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "Mecanum test")
 public class MecanumTest extends OpMode {
 
-    public DcMotor frontLeftDrive = null;
-    public DcMotor frontRightDrive = null;
-    public DcMotor rearLeftDrive = null;
-    public DcMotor rearRightDrive = null;
+    MecanumHardware hardware = new MecanumHardware();
 
-    public final double SLOW = 0.1;
-    public final double NORMAL = 0.2;
-    public final double FAST = 0.4;
-
-    public void init(){
-        frontLeftDrive = hardwareMap.dcMotor.get("fl_motor");
-        frontRightDrive = hardwareMap.dcMotor.get("fr_motor");
-        rearLeftDrive = hardwareMap.dcMotor.get("rl_motor");
-        rearRightDrive = hardwareMap.dcMotor.get("rr_motor");
-
-        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-
+    public void init() {
     }
 
     public void loop() {
 
         double driverSpeedMod;
 
-        if(gamepad1.left_bumper)        driverSpeedMod = FAST;
-        else if(gamepad1.right_bumper)  driverSpeedMod = SLOW;
-        else                            driverSpeedMod = NORMAL;
+        if(gamepad1.left_bumper)        driverSpeedMod = hardware.FAST;
+        else if(gamepad1.right_bumper)  driverSpeedMod = hardware.SLOW;
+        else                            driverSpeedMod = hardware.NORMAL;
 
         // Wheel speeds
         double frontLeft = driverSpeedMod;
@@ -53,10 +39,10 @@ public class MecanumTest extends OpMode {
         rearRight = ((-gamepad1.left_stick_y) - gamepad1.left_stick_x + gamepad1.right_stick_x);
 
         // Set the power
-        frontLeftDrive.setPower(frontLeft * driverSpeedMod);
-        frontRightDrive.setPower(frontRight * driverSpeedMod);
-        rearLeftDrive.setPower(rearLeft * driverSpeedMod);
-        rearRightDrive.setPower(rearRight * driverSpeedMod);
+        hardware.frontLeftDrive.setPower(frontLeft * driverSpeedMod);
+        hardware.frontRightDrive.setPower(frontRight * driverSpeedMod);
+        hardware.rearLeftDrive.setPower(rearLeft * driverSpeedMod);
+        hardware.rearRightDrive.setPower(rearRight * driverSpeedMod);
 
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
@@ -66,6 +52,7 @@ public class MecanumTest extends OpMode {
         telemetry.addData("frontRight", frontRight);
         telemetry.addData("rearLeft", rearLeft);
         telemetry.addData("rearRight", rearRight);
+        telemetry.addLine();
 
     }
 }
